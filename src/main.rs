588038306d3352f9f071668f738633fbe0262f0b
pinkforest <36498018+pinkforest@users.noreply.github.com>
2025-11-29 16:10:22 +1300

    handle reconnect

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,11 @@ use rustls::{ServerConfig, ServerConnection};
 use rustls_pki_types::pem::PemObject;
 use rustls_pki_types::{CertificateDer, PrivateKeyDer};
 use std::fs::File;
+use std::io::BufRead;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::path::Path;
 use std::sync::Arc;
-use std::io::BufRead;
 
 fn load_file(f: &'static str) -> Vec<u8> {
     let mut f = File::open(Path::new(f)).unwrap();
@@ -49,51 +49,56 @@ fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:8282").unwrap();
 
-    let (mut sock, addr) = listener.accept().unwrap();
-
-    let mut sconn = ServerConnection::new(Arc::new(rustls_config)).unwrap();
-
-    let out = "HTTP/1.1 200 OK\r\nContent-Length: 3\r\n\r\nUwU";
-    let mut req_received = false;
-    let mut sent_out = false;
-
     loop {
-        let incoming = sconn.read_tls(&mut sock).unwrap();
-        let io_state = sconn.process_new_packets().unwrap();
-        
-        println!("Incoming tls = {incoming}, io_state = {:?}", io_state);
-        
-        if io_state.peer_has_closed() {
-            println!("Peer goodbye.");
-            break;
-        }
-        
-        if !sconn.is_handshaking() {
-            let mut reader = sconn.reader();
-            //let mut rbuf: Vec<u8> = Vec::with_capacity(8192);
-            //let read_in = reader.read(&mut rbuf).unwrap();
+        println!("Waiting for a new connection..");
+        let (mut sock, addr) = listener.accept().unwrap();
 
-            match reader.fill_buf() {
-                Ok(rbuf) => {
-                    println!("Read in = {:?}", core::str::from_utf8(&rbuf));
-                    if rbuf.starts_with(b"GET") {
-                        req_received = true;
+        let mut sconn = ServerConnection::new(Arc::new(rustls_config.clone())).unwrap();
+
+        let out = "HTTP/1.1 200 OK\r\nContent-Length: 3\r\n\r\nUwU";
+        let mut req_received = false;
+        let mut sent_out = false;
+
+        loop {
+            let incoming = sconn.read_tls(&mut sock).unwrap();
+            let io_state = sconn.process_new_packets().unwrap();
+
+            println!("Incoming tls = {incoming}, io_state = {:?}", io_state);
+
+            if incoming == 0 {
+                break;
+            }
+
+            if io_state.peer_has_closed() {
+                println!("Peer goodbye.");
+                break;
+            }
+
+            if !sconn.is_handshaking() {
+                let mut reader = sconn.reader();
+
+                match reader.fill_buf() {
+                    Ok(rbuf) => {
+                        println!("Read in = {:?}", core::str::from_utf8(&rbuf));
+                        if rbuf.starts_with(b"GET") {
+                            req_received = true;
+                        }
                     }
+                    Err(e) => {}
                 }
-                Err(e) => {},
-            }                    
-        }
+            }
 
-        if req_received && !sent_out {
-            let mut writer = sconn.writer();
+            if req_received && !sent_out {
+                let mut writer = sconn.writer();
 
-            let total_out = writer.write(&out.as_bytes()).unwrap();
-            writer.flush().unwrap();
-            sent_out = true;
-        }
+                let total_out = writer.write(&out.as_bytes()).unwrap();
+                writer.flush().unwrap();
+                sent_out = true;
+            }
 
-        while sconn.wants_write() {
-            sconn.write_tls(&mut sock).unwrap();
+            while sconn.wants_write() {
+                sconn.write_tls(&mut sock).unwrap();
+            }
         }
     }
 }
